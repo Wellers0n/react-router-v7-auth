ad7299afe4e31385b08c35e1636e260d17d9fe09
Wellerson <wllrsn.c@gmail.com>
2025-07-07 00:25:50 -0300

    feat: add auth cookie

--- app/auth.ts
+++ app/auth.ts
@@ -1,5 +1,13 @@
 import { createCookie, redirect } from "react-router";
 
+const authCookie = createCookie("token", {
+  httpOnly: true,
+  path: "/",
+  sameSite: "lax",
+  secure: process.env.NODE_ENV === "production",
+  maxAge: 54000, // 15 hours
+});
+
 export const getCookies = (cookies: string, value: string) => {
   const cookieArray = cookies?.split("; ");
   const cookieObject: Record<string, string> = {};
